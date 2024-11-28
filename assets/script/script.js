017125472f5cfe116c18ceb3f288670e8d1d9651
ZhugeB1ao <doanhuynhgiabao0508@gmail.com>
2024-11-28 14:02:42 +0700

    first commit

---
+++ assets/script/script.js
@@ -0,0 +1,28 @@
+const buyTickets = document.querySelectorAll('.js-buy-ticket');
+const modal = document.querySelector('.js-modal');
+const closeModal = document.querySelector('.js-close-modal');
+const modalContent = document.querySelector('.js-modal-content');
+
+function showBuyTickets() {
+    modal.classList.add('open');
+}
+
+function hideBuyTickets() {
+    modal.classList.remove('open');
+}
+
+//for any buy ticket button is click => show modal
+for(const buyTicket of buyTickets) {
+    buyTicket.addEventListener('click', showBuyTickets);
+}
+
+// close modal
+closeModal.addEventListener('click', hideBuyTickets);
+
+// close if click outside of modal
+modal.addEventListener('click', hideBuyTickets);
+
+// except not close when click on modal
+modalContent.addEventListener('click', function (event) {
+    event.stopPropagation();
+});
